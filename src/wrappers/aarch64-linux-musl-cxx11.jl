c14bb63faf632ae92878f834a08583ef8c915118
jlbuild <juliabuildbot@gmail.com>
2021-07-06 12:05:43 +0000

    XGBoost_jll build 1.4.2+0

--- src/wrappers/aarch64-linux-musl-cxx11.jl
+++ src/wrappers/aarch64-linux-musl-cxx11.jl
@@ -2,89 +2,21 @@
 export libxgboost, xgboost
 
 using CompilerSupportLibraries_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-LIBPATH_default = ""
-
-# Relative path to `libxgboost`
-const libxgboost_splitpath = ["lib", "libxgboost.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libxgboost_path = ""
-
-# libxgboost-specific global declaration
-# This will be filled out by __init__()
-libxgboost_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libxgboost = "libxgboost.so"
-
-
-# Relative path to `xgboost`
-const xgboost_splitpath = ["bin", "xgboost"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-xgboost_path = ""
-
-# xgboost-specific global declaration
-function xgboost(f::Function; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
-    global PATH, LIBPATH
-    env_mapping = Dict{String,String}()
-    if adjust_PATH
-        if !isempty(get(ENV, "PATH", ""))
-            env_mapping["PATH"] = string(PATH, ':', ENV["PATH"])
-        else
-            env_mapping["PATH"] = PATH
-        end
-    end
-    if adjust_LIBPATH
-        LIBPATH_base = get(ENV, LIBPATH_env, expanduser(LIBPATH_default))
-        if !isempty(LIBPATH_base)
-            env_mapping[LIBPATH_env] = string(LIBPATH, ':', LIBPATH_base)
-        else
-            env_mapping[LIBPATH_env] = LIBPATH
-        end
-    end
-    withenv(env_mapping...) do
-        f(xgboost_path)
-    end
-end
-
-
-# Inform that the wrapper is available for this platform
-wrapper_available = true
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("XGBoost")
+JLLWrappers.@declare_library_product(libxgboost, "libxgboost.so")
+JLLWrappers.@declare_executable_product(xgboost)
 function __init__()
-    # This either calls `@artifact_str()`, or returns a constant string if we're overridden.
-    global artifact_dir = find_artifact_dir()
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll)
+    JLLWrappers.@init_library_product(
+        libxgboost,
+        "lib/libxgboost.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    global PATH_list, LIBPATH_list
-    # Initialize PATH and LIBPATH environment variable listings
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (CompilerSupportLibraries_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (CompilerSupportLibraries_jll.LIBPATH_list,))
+    JLLWrappers.@init_executable_product(
+        xgboost,
+        "bin/xgboost",
+    )
 
-    global libxgboost_path = normpath(joinpath(artifact_dir, libxgboost_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libxgboost_handle = dlopen(libxgboost_path, RTLD_LAZY | RTLD_DEEPBIND)
-    push!(LIBPATH_list, dirname(libxgboost_path))
-
-    global xgboost_path = normpath(joinpath(artifact_dir, xgboost_splitpath...))
-
-    push!(PATH_list, dirname(xgboost_path))
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
