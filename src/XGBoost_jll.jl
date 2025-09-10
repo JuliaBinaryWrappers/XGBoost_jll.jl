945af11f03fd32fa00eb8381dccf64a2639a3f1a
jlbuild <juliabuildbot@gmail.com>
2025-09-10 06:01:01 +0000

    XGBoost_jll build 2.1.4+0

--- src/XGBoost_jll.jl
+++ src/XGBoost_jll.jl
@@ -2,8 +2,6 @@
 baremodule XGBoost_jll
 using Base
 using Base: UUID
-using LazyArtifacts
-Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("XGBoost")
